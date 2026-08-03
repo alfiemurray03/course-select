35dd91cf768b68eceb79a66f7f60ead5f54cd168
ALFIE THOMAS HOLYWOOD MURRAY <232256186+alfiemurray03@users.noreply.github.com>
2026-08-03 23:06:05 +0100

    Connect Aptenvo basket provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { BasketProvider } from './basket';
 import './styles.css';
 import './catalogue.css';
+import './basket.css';
 
 const root = document.getElementById('app');
 if (!root) throw new Error('Application root not found.');
@@ -11,7 +13,9 @@ if (!root) throw new Error('Application root not found.');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <BasketProvider>
+        <App />
+      </BasketProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
