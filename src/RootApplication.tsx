815df47156ecc75621ac09d764912eeb15458677
ALFIE THOMAS HOLYWOOD MURRAY <232256186+alfiemurray03@users.noreply.github.com>
2026-08-08 03:31:25 +0100

    Route LMS course pages through proper programme player

--- src/RootApplication.tsx
+++ src/RootApplication.tsx
@@ -3,16 +3,26 @@ import AccessibilityTools from './AccessibilityTools';
 import AgeGate from './AgeGate';
 import LearningPlatformRouter from './LearningPlatformRouter';
 import ProductionSubscriptionCheckoutPage from './ProductionSubscriptionCheckoutPage';
+import ProgrammeCoursePlayer from './ProgrammeCoursePlayer';
 
 export default function RootApplication() {
   const path = useLocation().pathname;
   const subscription = path.match(/^\/lms\/subscribe\/([^/]+)$/);
+  const course = path.match(/^\/lms\/course\/([^/]+)$/);
 
   if (subscription) {
     return <>
       <AgeGate />
       <AccessibilityTools />
       <ProductionSubscriptionCheckoutPage planId={decodeURIComponent(subscription[1])} />
+    </>;
+  }
+
+  if (course) {
+    return <>
+      <AgeGate />
+      <AccessibilityTools />
+      <ProgrammeCoursePlayer slug={decodeURIComponent(course[1])} />
     </>;
   }
 
